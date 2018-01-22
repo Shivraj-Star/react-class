52d991167a16a967eb19dff6b499a20410f607ff
Balwant Padwal <Balwant.Padwal@synechron.com>
2018-01-22 17:18:27 +0530

    Added first commit

---
+++ client/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import {BasicComponent} from './basic.component';
+
+//if we write export 'default' keyword no need to write into {} parenthesis
+import BasicComponent from './basic.component';
+
+
+
+ReactDOM.render(<BasicComponent />,document.getElementById('content'));
---
+++ client/basic.component.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// Code Here
+export default class BasicComponent extends React.Component{
+    render(){
+            return( <div> <h1>H1 component using JSX ES6</h1>
+            <h2>H1 component using JSX ES6</h2>
+            <h3>H1 component using JSX ES6</h3>
+            </div>)
+     }
+}
